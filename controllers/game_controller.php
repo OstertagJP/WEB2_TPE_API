e3a1679fa4a6c8a6283f20eb954529d9937b6aa8
GermanSilva88 <130848764+GermanSilva88@users.noreply.github.com>
2023-06-03 15:34:52 -0300

    avanzo

--- controllers/game_controller.php
+++ controllers/game_controller.php
@@ -5,8 +5,15 @@ require_once 'views/game_view.php';
 
 class game_controller
 {
+
+    private $model;
+    private $view;
+
     //Acá hay que iniciar los atributos y el contructor
     function __construct()
     {
+
+        $this->model = new game_model();
+        $this->view = new game_view();
     }
 }
