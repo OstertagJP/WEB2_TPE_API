2e5475eb609265974c86f022b350f5b4544a1b26
Juan Pablo Ostertag <figuta@gmail.com>
2023-06-05 18:53:15 -0300

    Modificacion game_model

--- models/game_model.php
+++ models/game_model.php
@@ -17,21 +17,31 @@ class game_model{
         return $games;
     }
 
-    function insert_game($name_game, $description_game){                //Funcion que inserta un juego nuevo (se le pasan dos parametros, name y description)
+    function insert_game($name_game, $description_game, $genre_id){                //Funcion que inserta un juego nuevo (se le pasan tres parametros, name, description y genre)
         // 2. EJECUTAR CONSULTA SQL (2 SUBPASOS: PREPARE Y EXECUTE)
-        $query = $this->db->prepare('INSERT INTO game (name_game, description_game) VALUES (?,?)');
-        $query -> execute(array($name_game, $description_game));
+        $query = $this->db->prepare('INSERT INTO game (name_game, description_game, genre_id) VALUES (?,?,?)');
+        $query -> execute(array($name_game, $description_game, $genre_id));
         
     }
     
     function delete_game($id_game){                                      //Funcion que elimina un juego (se le pasa un parametro, id)
         // 2. EJECUTAR CONSULTA SQL (2 SUBPASOS: PREPARE Y EXECUTE)
-        $query = $this->db->prepare('DELETE FROM game WHERE id = ?');
+        $query = $this->db->prepare('DELETE FROM game WHERE id_game = ?');
         $query -> execute(array($id_game));
 
     }
 
-                                  // FALTA LA FUNCION PARA MODIFICAR UN JUEGO
+    function get_games_by_genre($genre){
+        // 2. EJECUTAR CONSULTA SQL (2 SUBPASOS: PREPARE Y EXECUTE)        //Funcion que obtiene todos los juegos de un genero en particular
+        $query = $this->db->prepare('SELECT * FROM game WHERE genre_id = ?');
+        $query->execute(array($genre));
+        $games=$query->fetchAll(PDO::FETCH_OBJ);
+        return $games;
+    }
+
+    function update_game($id_game, $name_game, $description_game, $genre_id){          //Funcion que modifica un juego ya cargado. Se le pasan los cuatro parametros.
+
+    }                              
     
 
 }
