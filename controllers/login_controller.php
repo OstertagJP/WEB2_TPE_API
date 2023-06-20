370a4b2e9b29c17957fcac1eff69af189327e971
Juan Pablo Ostertag <figuta@gmail.com>
2023-06-20 16:22:48 -0300

    agrego auth helper

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -1,37 +1,42 @@
 <?php
-
-require_once('views/login_view.php');
+require_once 'views/login.view.php';
+require_once 'models/user.model.php';
+require_once 'helpers/auth.helper.php';
 
 class login_controller {
 
     private $view;
     private $model;
+    private $auth_helper;
 
     public function __construct() {
         $this->view = new login_view();
         $this->model = new user_model();
+        $this->auth_helper = new auth_helper();
     }
 
     public function show_login() {
         $this->view->show_login_view();
     }
 
-    public function verify_user() {
+    public function verifyUser() {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $user = $this->model->get_by_username($username);         
+        $user = $this->model->get_by_username($username);
 
-        if (!empty($user) && password_verify($password, $user->password)) {  // encontró un user con el username que mandó, y tiene la misma contraseña
-            //INICIO LA SESSION Y LOGUEO AL USUARIO...
-            session_start();
-            $_SESSION['ID_USER'] = $user->id_user;
-            $_SESSION['USERNAME'] = $user->email;
-            header('Location: home');   //aca lo mandamos a home, PERO YA LOGUEADO
+        // encontró un user con el username que mandó, y tiene la misma contraseña
+        if (!empty($user) && password_verify($password, $user->password)) {
+            $this->auth_helper->login($user);
+
+            header('Location: home');
         } else {
             $this->view->show_login_view("Login incorrecto");
         }
-       
     }
 
+    public function logout() {
+        $this->auth_helper->logout();
+        header('Location: ' . LOGIN);
+    }
 }
